e0869e56faebe8b278525ebd696138807bec5758
JJ Kampf <kampfj@sas.upenn.edu>
2019-07-22 19:48:08 -0600

    Initial Commit

---
+++ Main.java
@@ -0,0 +1,43 @@
+import java.util.List;
+
+public class Main {
+    
+    public static void main(String[] args) {
+
+        List<Participant> participants = new NameFileReader(args[0]).getParticipants();
+        // This is the number of rooms that will be constructed. Feel free to 
+        // change this, it's currently set to a number assuming each participnat is 
+        // placed in a room. 
+        int numRooms = participants.size() / Integer.parseInt(args[2]);
+        if (args[1].equals("preferences")) {
+            ConstructRooms finalRooms = new ConstructRooms(participants, 
+                                    numRooms);
+            for (Room room : finalRooms.res) {
+                System.out.println(room);
+            }
+        } else {
+            NoPreferences pref = new NoPreferences(participants, numRooms, 
+                                            Integer.parseInt(args[2]));
+            for (Room room : pref.getRooms()) {
+                System.out.println(room);
+            }
+        }
+        
+        
+    }
+
+    private static void print(List<Participant> participants) {
+        for (Participant p : participants) {
+            System.out.print(p + ": ");
+            for (Participant p1 : p.getPreferences()) {
+                System.out.print("+" + p1 + " ");
+            }
+            for (Participant p1 : p.getDisrequests()) {
+                System.out.print("-" + p1 + " ");
+            }
+            System.out.println();
+        }
+    }
+
+}
+
